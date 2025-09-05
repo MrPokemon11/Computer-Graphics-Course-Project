b75157e79fe846ac5e7ce56f0188d55c4e0407b7
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2025-09-05 16:03:42 -0400

    Basic Movement System

---
+++ Assets/Scripts/Player Movement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private bool canJump = true;
+
+    private float MoveSpeed = 5f;
+    float JumpForce = 10f;
+    
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move left
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * (MoveSpeed * Time.deltaTime);
+            
+        } 
+        //Move Right
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * (MoveSpeed * Time.deltaTime);
+        }
+        //Jump
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && canJump == true)
+        {
+            rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            canJump = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+}
