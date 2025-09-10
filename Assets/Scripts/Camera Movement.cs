862ab9e3e49b1c7930e8f6207bf73557b63b9d47
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2025-09-10 18:03:56 -0400

    work on camera movement

--- Assets/Scripts/Camera Movement.cs
+++ Assets/Scripts/Camera Movement.cs
@@ -15,6 +15,7 @@ public class CameraMovement : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime);
+        
         if (transform.position.x <= 4.5f)
         {
             transform.position = new Vector3(4.5f, transform.position.y, transform.position.z);
