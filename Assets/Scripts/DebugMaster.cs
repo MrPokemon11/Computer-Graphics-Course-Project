110662aee0be859dad4b6d8b4d0876982fc3f371
MrPokemon11 <53610352+MrPokemon11@users.noreply.github.com>
2025-11-28 09:15:34 -0500

    shuffle things around, start on debug script

--- Assets/Scripts/DebugMaster.cs
+++ Assets/Scripts/DebugMaster.cs
@@ -2,14 +2,30 @@ using UnityEngine;
 
 public class DebugMaster : MonoBehaviour
 {
+    Renderer rend;
+    Material m_Material;
+    Material m_NoTextureMaterial;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        m_Material = rend.material;
+        m_NoTextureMaterial = Resources.Load<Material>("NoTexture");
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void ToggleTextures()
+    {
+        
+    }
+
+    void CycleLUT()
     {
         
     }
